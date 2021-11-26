0487cbd8e18a11bd8192dfe9de6a33a83b283e21
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-26 17:34:50 +0100

    adds $_GET function to get the word, str_replace to replace it and strlen to print the paragraph length

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php 
 
+$testo = "<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>";
+
+$_GET["parolaDaCensurare"];
+
+/* var_dump($_GET); */
+
+$testoDue = str_replace($_GET, "***", $testo);
+
+echo $testoDue;
+
+echo "<br> questo paragrafo è lungo ", strlen($testo), " lettere";
+
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
